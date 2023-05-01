f74aff3dd9aa9d8d5a2832212d6892405eca0a4a
deeNair <dee.kurup@gmail.com>
2023-05-01 11:39:43 -0400

    form

--- src/components/AddJobs.js
+++ src/components/AddJobs.js
@@ -1,20 +1,57 @@
 import React from 'react';
-import { Table,TableBody, TableCell, TableHead, TableRow } from '@mui/material';
-import { useEffect,useState } from 'react';
+import { FormGroup, FormControl, InputLabel, Input, Button, styled, Typography } from '@mui/material';
+import { useState } from 'react';
+
+const initialValue = {
+    company: '',
+    position: '',
+    jobstatus: '',
+    recruiterEmail: ''
+  }
+  const Container = styled(FormGroup)`
+    width: 50%;
+    margin: 5% 0 0 25%;
+    & > div {
+        margin-top: 20px;
+`;
+const onValueChange = (e) => {
+      
+    console.log(e.target.name,e.target.value);
+    //setJobs({...jobs,[e.target.name]:e.target.value})
+   //console.log(jobs);
+ }
+ //take all the details in form(stored in jobs variable and setjob add value entered in form to it) and send to api
+ const addJobsDetails = async() => {
+    //call the api  
+    //await addJobs(jobs);
+      //navigate('/all');
+  }
 
 const AddJobs = () => {
+const [jobs, setJobs] = useState(initialValue);
   return (
-    <Table>
-    <TableHead>
-        <TableRow>
-        <TableCell>Id</TableCell>
-        <TableCell>Company</TableCell>
-        <TableCell>Position</TableCell>
-        <TableCell>Jobstatus</TableCell>
-        <TableCell>RecruiterEmail</TableCell>
-       </TableRow>
-    </TableHead>
-    </Table>
+    <Container>
+      <Typography variant="h4">Add jobs</Typography>
+      <FormControl>
+        <InputLabel>Company</InputLabel>
+        <Input onChange={(e)=>onValueChange(e)} name="company"/>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Position</InputLabel>
+        <Input onChange={(e)=>onValueChange(e)} name="position"/>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Jobstatus</InputLabel>
+        <Input onChange={(e)=>onValueChange(e)} name="jobstatus"/>
+      </FormControl>
+      <FormControl>
+        <InputLabel>RecruiterEmail</InputLabel>
+        <Input onChange={(e)=>onValueChange(e)} name="recruiterEmail"/>
+      </FormControl>
+      <FormControl>
+        <Button variant='contained' onClick={()=>addJobsDetails()}>Add jobs</Button>
+      </FormControl>
+    </Container>
   )
 }
 
--- src/components/JobHistory.js
+++ src/components/JobHistory.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import {Table,TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+
+//import { useEffect,useState } from 'react';
 
 const JobHistory = () => {
   return (
-    <div><h1>JobHistory</h1></div>
+    <Table>
+    <TableHead>
+        <TableRow>
+        <TableCell>Id</TableCell>
+        <TableCell>Company</TableCell>
+        <TableCell>Position</TableCell>
+        <TableCell>Jobstatus</TableCell>
+        <TableCell>RecruiterEmail</TableCell>
+       </TableRow>
+    </TableHead>
+    </Table>
   )
 }
 
